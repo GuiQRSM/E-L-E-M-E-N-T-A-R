82c82ecefe68fa814aac35211b5c3e55e55b1ddc
GuiQRSM <endbeob15@gmail.com>
2021-11-20 13:26:17 -0300

    It implements a 'Dart file' called 'LayoutStful' inside the 'lib' folder and in this same rescem file it declares a stful that returns a Scaffold() class.

---
+++ lib/LayoutStful.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LayoutStful extends StatefulWidget {
+  const LayoutStful({Key? key}) : super(key: key);
+
+  @override
+  _LayoutStfulState createState() => _LayoutStfulState();
+}
+
+class _LayoutStfulState extends State<LayoutStful> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
