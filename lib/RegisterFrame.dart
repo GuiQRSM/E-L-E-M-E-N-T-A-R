41d25ceae7aa95bd1a88bbee6e0176375214464c
GuiQRSM <endbeob15@gmail.com>
2021-11-30 12:06:20 -0300

    Declare the const primeColor to set the widgets' colors, and call the appBar attribute inside the Scaffold() class returned by the stful framework.

--- lib/RegisterFrame.dart
+++ lib/RegisterFrame.dart
@@ -7,11 +7,20 @@ class RegisterFrame extends StatefulWidget {
   _RegisterFrameState createState() => _RegisterFrameState();
 }
 
+var primeColor = Color.fromARGB(200, 89, 8, 105);
+
 class _RegisterFrameState extends State<RegisterFrame> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("E L E M E N T A R",
+          style: TextStyle(
+              fontWeight: FontWeight.w800,
+              color: Color.fromARGB(250, 209, 152, 207),
+          ),),
+        backgroundColor: primeColor,
+      ),
     );
   }
 }
