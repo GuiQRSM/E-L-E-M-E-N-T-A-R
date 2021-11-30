253ac9544428e75d33756e6436a4d5541d150760
GuiQRSM <endbeob15@gmail.com>
2021-11-30 11:57:01 -0300

    It implements in the lib folder a new dart file named RegiterFrame, where in this same file a stful structure is declared that returns a Scaffold class.

---
+++ lib/RegisterFrame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterFrame extends StatefulWidget {
+  const RegisterFrame({Key? key}) : super(key: key);
+
+  @override
+  _RegisterFrameState createState() => _RegisterFrameState();
+}
+
+class _RegisterFrameState extends State<RegisterFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
