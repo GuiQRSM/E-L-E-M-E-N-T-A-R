0709587d7c6d320c2c66d584d1eb1c8e333407b6
GuiQRSM <endbeob15@gmail.com>
2021-11-30 12:35:24 -0300

    Declare and configure in the body of the Scaffold() class returned in the stful structure, a Padding() to receive the Text() class that represents the screen title and call a TextField() class to configure the data field for the email .

--- lib/RegisterFrame.dart
+++ lib/RegisterFrame.dart
@@ -10,6 +10,9 @@ class RegisterFrame extends StatefulWidget {
 var primeColor = Color.fromARGB(200, 89, 8, 105);
 
 class _RegisterFrameState extends State<RegisterFrame> {
+
+  TextEditingController _controllerEmail = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +25,37 @@ class _RegisterFrameState extends State<RegisterFrame> {
         backgroundColor: primeColor,
       ),
       body: Container(
-        padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(28),
         width: double.infinity,
         child: Column(
           children: <Widget>[
-
+            Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 15),
+            child: Text(
+              "-s i n g  o u t-",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w500,
+                color: Color.fromARGB(250, 171, 9, 128),
+              ),),
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: primeColor,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: primeColor,
+              ),
+              controller: _controllerEmail,
+            ),
           ],
         ),
       ),
