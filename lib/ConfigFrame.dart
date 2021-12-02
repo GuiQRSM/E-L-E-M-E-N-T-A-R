fc1850d11617b3fd720a830360a5e85d2bcce7a8
GuiQRSM <endbeob15@gmail.com>
2021-12-02 12:05:51 -0300

    Implements a new dart file named ConfigFrame in the lib folder, which is already configured with stful structure, returning Scaffold().

---
+++ lib/ConfigFrame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConfigFrame extends StatefulWidget {
+  const ConfigFrame({Key? key}) : super(key: key);
+
+  @override
+  _ConfigFrameState createState() => _ConfigFrameState();
+}
+
+class _ConfigFrameState extends State<ConfigFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
