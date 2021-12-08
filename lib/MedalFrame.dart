cd64568af182ffea9282e4c817fab3d9602c717b
GuiQRSM <endbeob15@gmail.com>
2021-12-08 13:18:27 -0300

    Implement in 'children: <Widget> [ ]' of class 'Column()' a class 'Padding()' that receives as 'child:,' the class 'Row()' that receives in its 'childre: <Widget> [ ]' the 'GestureDetector()' and 'Text()' classes and also implement the 'Navigator.pop()' class in the 'onTap:,' attribute of 'GestureDetector()'.

--- lib/MedalFrame.dart
+++ lib/MedalFrame.dart
@@ -18,7 +18,7 @@ class _MedalFrameState extends State<MedalFrame> {
       body: Container(
         width: double.infinity,
         child: SingleChildScrollView(
-          padding: EdgeInsets.all(24),
+          padding: EdgeInsets.all(32),
           child: Column(
             children: <Widget>[
               Padding(
@@ -48,6 +48,32 @@ class _MedalFrameState extends State<MedalFrame> {
                     color: primeColor,
                     letterSpacing: 5,
                   ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 100),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset(
+                        "Type_Meday/backbutton.png",
+                        height: 90,
+                      ),
+                    ),
+                    Text(
+                        "VOLTAR",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        color: primeColor,
+                        letterSpacing: 5,
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
