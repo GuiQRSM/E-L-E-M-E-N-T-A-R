9cc82079e635c907c897a6c58dc57450ee5dd08a
GuiQRSM <endbeob15@gmail.com>
2021-12-08 12:29:07 -0300

    Implement in lib folder the dart file 'MedalFrame' with 'material.dart' already imported and the stful structure with class 'MedalFrame' which extends from 'StatefulWidget' already called.

---
+++ lib/MedalFrame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedalFrame extends StatefulWidget {
+  const MedalFrame({Key? key}) : super(key: key);
+
+  @override
+  _MedalFrameState createState() => _MedalFrameState();
+}
+
+class _MedalFrameState extends State<MedalFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
